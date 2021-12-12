774a9f074575ff5c9bc64c7f0f26a3a3b21e82ea
kekvir <kekvir@ttu.ee>
2021-12-12 02:38:17 +0200

    sign in page works

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface SignInForm {
+  email: String,
+  password: String
+}
+
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
+  hide = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  signIn({email, password }: SignInForm) {
+     if(!email || !password) {
+       return;
+     }
   }
 
 }
